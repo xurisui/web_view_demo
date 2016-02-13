6c9c42c0621cfb0cd2ccfedd6c2a8614b6d02bb0
sunziqiang <sunziqiang@wecash.net>
2016-02-14 01:04:59 +0800

    ddd

---
+++ web_view/web_view/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  web_view
+//
+//  Created by sun on 16/2/14.
+//  Copyright © 2016年 sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ web_view/web_view/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  web_view
+//
+//  Created by sun on 16/2/14.
+//  Copyright © 2016年 sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
